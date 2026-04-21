0b0d3cf2c85d81d22c256365f495fb5120439796
Ben Hoover <benjamin_hoover@icloud.com>
2026-04-21 14:23:50 -0600

    added an assets tab

--- sketch.js
+++ sketch.js
@@ -1,8 +1,9 @@
-// Project Title
-
+// Undertale
+// Ben H and Zeyad M
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+
 
 
 function setup() {
@@ -17,23 +18,26 @@ function preload() {
   redHeartImg = loadImage("red-heart.png");
 }
 
-function fight(fightBorderSize, heartSize, x, y, speed) { //Foo's Function DO NOT TOUCH
-  fightBorderSize = 400;
-  heartSize = 30;
-  x = width/2;
-  y = height/2;
-  speed = 5;
 
-  x = constrain(x, width/2 - fightBorderSize/2, width/2 + fightBorderSize/2);
-  y = constrain(y, height/2 - fightBorderSize/2, height/2 + fightBorderSize/2);
+// What is you doing bruh 
 
-  function draw() {
-    background(0);
-    strokeWeight(20);
-    stroke(255);
-    noFill();
-    rect(width/2 - fightBorderSize/2, height/2 - fightBorderSize/2, fightBorderSize, fightBorderSize);
+// function fight(fightBorderSize, heartSize, x, y, speed) { //Foo's Function DO NOT TOUCH
+//   fightBorderSize = 400;
+//   heartSize = 30;
+//   x = width/2;
+//   y = height/2;
+//   speed = 5;
+
+//   x = constrain(x, width/2 - fightBorderSize/2, width/2 + fightBorderSize/2);
+//   y = constrain(y, height/2 - fightBorderSize/2, height/2 + fightBorderSize/2);
+
+//   function draw() {
+//     background(0);
+//     strokeWeight(20);
+//     stroke(255);
+//     noFill();
+//     rect(width/2 - fightBorderSize/2, height/2 - fightBorderSize/2, fightBorderSize, fightBorderSize);
     
-    image(redHeartImg, x - heartSize/2, y - heartSize/2, heartSize, heartSize);
-  }
-}
+//     image(redHeartImg, x - heartSize/2, y - heartSize/2, heartSize, heartSize);
+//   }
+// }
