3cbfb99bb215bd60e10a871a38d1c6f1e8c8a945
6mnyb2wtxp-code <6mnyb2wtxp@privaterelay.appleid.com>
2026-04-21 11:03:34 -0600

    made a fight function but not working

--- sketch.js
+++ sketch.js
@@ -10,6 +10,30 @@ function setup() {
 }
 
 function draw() {
-  background(220);
-  square(mouseX, mouseY,50);
+  background(0);
+}
+
+function preload() {
+  redHeartImg = loadImage("red-heart.png");
+}
+
+function fight(fightBorderSize, heartSize, x, y, speed) { //Foo's Function DO NOT TOUCH
+  fightBorderSize = 400;
+  heartSize = 30;
+  x = width/2;
+  y = height/2;
+  speed = 5;
+
+  x = constrain(x, width/2 - fightBorderSize/2, width/2 + fightBorderSize/2);
+  y = constrain(y, height/2 - fightBorderSize/2, height/2 + fightBorderSize/2);
+
+  function draw() {
+    background(0);
+    strokeWeight(20);
+    stroke(255);
+    noFill();
+    rect(width/2 - fightBorderSize/2, height/2 - fightBorderSize/2, fightBorderSize, fightBorderSize);
+    
+    image(redHeartImg, x - heartSize/2, y - heartSize/2, heartSize, heartSize);
+  }
 }
