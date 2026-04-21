c32d424e5c8ef906a9e72d6e2519c95f5d78195e
Ben Hoover <benjamin_hoover@icloud.com>
2026-04-21 15:09:17 -0600

    added the start cutscene

--- sketch.js
+++ sketch.js
@@ -4,20 +4,77 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-
+let cutscene;
+let title;
+let titleNumber = 1;
+let gameState = "cutscene";
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(640, 480);
 }
 
 function draw() {
   background(0);
+  playCutscene();
+  startTitle();
+}
+
+function keyPressed(){
+  if (key === "z" && gameState === "cutscene"){
+    titleNumber += 1;
+  }
+  if (titleNumber > 10){
+    gameState = "title";
+  }
 }
 
 function preload() {
-  redHeartImg = loadImage("red-heart.png");
+  title = loadImage("assets/title sprites/undertale-title-3.png");
+  // redHeartImg = loadImage("red-heart.png");
+  if (titleNumber === 1){
+    cutscene = loadImage("assets/title sprites/undertale-title-1.png");
+  }
+  if (titleNumber === 2){
+    cutscene = loadImage("assets/title sprites/undertale-title-2.png");
+  }
+  if (titleNumber === 3){
+    cutscene = loadImage("assets/title sprites/undertale-title-3.png");
+  }
+  if (titleNumber === 4){
+    cutscene = loadImage("assets/title sprites/undertale-title-4.png");
+  }
+  if (titleNumber === 5){
+    cutscene = loadImage("assets/title sprites/undertale-title-5.png");
+  }
+  if (titleNumber === 6){
+    cutscene = loadImage("assets/title sprites/undertale-title-6.png");
+  }
+  if (titleNumber === 7){
+    cutscene = loadImage("assets/title sprites/undertale-title-7.png");
+  }
+  if (titleNumber === 8){
+    cutscene = loadImage("assets/title sprites/undertale-title-8.png");
+  }
+  if (titleNumber === 9){
+    cutscene = loadImage("assets/title sprites/undertale-title-9.png");
+  }
+  if (titleNumber === 10){
+    cutscene = loadImage("assets/title sprites/undertale-titlescreen.png");
+  }
 }
 
+function playCutscene(){
+  if (gameState === "cutscene"){
+    image(cutscene, 0, 0, 640, 480);
+    preload();    
+  }
+}
+
+function startTitle(){
+  if (gameState === "title"){
+    image(title, 0, 0, 640, 480);
+  }
+}
 
 // What is you doing bruh 
 
