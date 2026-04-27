27b8b5c93f19e1bbc001405af46f06ce97da0768
Ben Hoover <benjamin_hoover@icloud.com>
2026-04-26 18:23:00 -0600

    added the name screen, added a confirm screen,  small changes to the title cutscene

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+
 // Undertale
 // Ben H and Zeyad M
 //
@@ -5,7 +6,8 @@
 // - describe what you did to take this project "above and beyond"
 
 //GAMESTATE
-let gameState = "ruins";
+let gameState = "title";
+let menuState = "instruction"
 
 //player variables
 let playerX, playerY;
@@ -36,12 +38,13 @@ let ruinsMusic;
 //SFX
 let undertaleBoom;
 let textSound;
+let musCymbal;
 
 //FONTS
 let determinationFont;
 
 //cutscene variables
-let scrollCutY = -682;
+let scrollCutY = -1023;
 let currentFrame = 0;
 let frameDuration = 300;
 let frameTimer = 0;
@@ -59,7 +62,6 @@ let cutsceneDialogue = [
 let showText = "";
 let charIndex = 0;
 let textSpeed = 4;
-let playerName;
 let cutscene = [];
 let title;
 let titleNumber = 1;
@@ -81,18 +83,42 @@ let selection = 0;
 let mapSize = 12;
 let ruinsMap;
 
-function setup() {
-  userStartAudio();
+let playersName = "";
+let letterJitterX = [];
+let letterJitterY = [];
+let nameJitterX = 0;
+let nameJitterY = 0;
+let selectedLetter = 0;
+let letterCols = 7;
 
+let capitals = [
+  "A","B","C","D","E","F","G","H","I",
+  "J","K","L","M","N","O","P","Q","R",
+  "S","T","U","V","W","X","Y","Z",
+];
+let lowercase = [
+  "a","b","c","d","e","f","g","h","i",
+  "j","k","l","m","n","o","p","q","r",
+  "s","t","u","v","w","x","y","z",
+]
+let letters = capitals.concat(lowercase);
+let buttons = ["Quit", "Backspace", "Done"];
+let totalSlots = letters.length + buttons.length;
+
+let confirmSelection = 0;
+let textSizeIncrease = 33;
+
+let playerNameMoveY = 0;
+let playerNameMoveReady = false;
+
+let walls = [];
+
+function setup() {
+  noSmooth();
   createCanvas(640 * 1.5, 480 * 1.5); 
   //createCanvas(windowWidth, windowHeight);
 
-  noSmooth();
-
-  textSound.setVolume(0.2);
-  onceUponATime.setVolume(oUATStartingVolume);
-  startMenuTheme.setVolume(0.5);
-  ruinsMusic.setVolume (0.5);
+  setupSound();
 
   x = width/2;
   y = height/2;
@@ -102,9 +128,17 @@ function setup() {
   currentSprites = playerSpriteFront;
 
   screenPosY = -height * (mapSize - 5);
+
+  for (let i = 0; i < letters.length; i++){
+    letterJitterX.push(0);
+    letterJitterY.push(0);
+  }
+
+  setupWalls();
 }
 
 function draw() {
+  noSmooth();
   if (gameState === "start"){
     clickToStart();
   }
@@ -151,6 +185,7 @@ function preload() {
   //sfx
   undertaleBoom = loadSound("assets/sound effects/undertale.mp3");
   textSound = loadSound("assets/sound effects/SND_TXT2.wav");
+  musCymbal = loadSound("assets/sound effects/mus-cymbal.mp3");
 
   ruinsMap = loadImage("assets/map sprites/ruins-1.png");
   determinationFont = loadFont("assets/fonts/determination.otf");
@@ -164,17 +199,27 @@ function preload() {
   cutscene.push(loadImage("assets/title sprites/undertale-titlescreen.png"));
 }
 
-function clickToStart(){
-  background(0);
-  textFont(determinationFont);
-  fill(255);
-  textSize(40);
-  text("Click To Start", width /5, height /4);  
+function setupSound(){
+  userStartAudio();
+  textSound.setVolume(0.2);
+  onceUponATime.setVolume(oUATStartingVolume);
+  startMenuTheme.setVolume(0.5);
+  ruinsMusic.setVolume (0.5);
 }
 
+
+
 function keyPressed() {
+
+  if (gameState === "title" && menuState === "instruction"){
+    if (keyCode === 90 || keyCode === ENTER){
+      menuState = "name";
+      return;
+    }
+  }
+
   if (currentFrame === 11){
-    if (keyCode === 90){
+    if (keyCode === 90 || keyCode === ENTER){
       gameState = "title";
     }
   }
@@ -183,6 +228,28 @@ function keyPressed() {
       onceUponATime.stop();
       currentFrame = 11;
     }
+  }
+  
+  if (menuState === "confirm"){
+    if (keyCode === LEFT_ARROW){
+      confirmSelection = 0;
+    }
+    if (keyCode === RIGHT_ARROW){
+      confirmSelection = 1;
+    }
+    if (keyCode === 90 || keyCode === ENTER){
+      if (confirmSelection === 1){
+        startMenuTheme.stop();
+        fadeAlpha = 0;
+        menuState = "startinggame"
+      }
+      else{
+        menuState = "name";
+        playerNameMoveReady = false;
+        textSizeIncrease = 33;
+      }
+    }
+    return;
   }
 
   if (keyCode === LEFT_ARROW && selection > 0 && choice === 0 && gameState === "chooseWhatToDoWithEnemy") {
@@ -197,8 +264,92 @@ function keyPressed() {
   if (keyCode === 115) { // doesnt work for some reason 
     fullscreen();
   }
-}
 
+  if (menuState === "name"){
+    let onButton = selectedLetter >= letters.length;
+
+    if (keyCode === RIGHT_ARROW){
+      if (onButton){
+        selectedLetter = min(selectedLetter + 1, letters.length + buttons.length - 1)
+      }
+      else{
+        selectedLetter = min(selectedLetter + 1, letters.length - 1);
+      }
+    }
+
+    if (keyCode === LEFT_ARROW){
+      if (onButton){
+        selectedLetter = max(selectedLetter - 1, letters.length);
+      }
+      else{
+        selectedLetter = max(selectedLetter - 1, 0);
+      }
+    }
+
+    if (keyCode === DOWN_ARROW){
+      if (!onButton){
+        let nextIndex = selectedLetter + letterCols;
+
+        if (nextIndex >= letters.length){
+          selectedLetter = letters.length;
+        }
+        else if(selectedLetter < capitals.length && nextIndex >= capitals.length){
+          let currentCol = selectedLetter % letterCols;
+          selectedLetter = capitals.length + currentCol;
+        }
+        else{
+          selectedLetter = nextIndex;
+        }
+      }
+    }
+
+    if (keyCode === UP_ARROW){
+      if (onButton){
+        selectedLetter = letters.length - 1;
+      }
+      else{
+        let prevIndex = selectedLetter - letterCols;
+
+        if (selectedLetter >= capitals.length && selectedLetter < capitals.length + letterCols){
+          let lastCapRow = Math.floor((capitals.length - 1) / letterCols);
+          let currentCol = (selectedLetter - capitals.length) % letterCols;
+          let target = lastCapRow * letterCols + currentCol;
+          selectedLetter = lastCapRow * letterCols + currentCol;
+          selectedLetter = min(target, capitals.length - 1)
+        }
+        else{
+          selectedLetter = max(prevIndex, 0);
+        }
+      }
+    }
+
+    if (keyCode === 90 || keyCode === ENTER){
+      if (onButton){
+        let buttonIndex = selectedLetter - letters.length;
+        if (buttonIndex === 0){
+          menuState = "instruction";
+          selectedLetter = 0;
+          playersName = "";
+        }
+        if (buttonIndex === 1){
+          playersName = playersName.slice(0, -1);
+        }
+        if (buttonIndex === 2){
+          if (playersName.length > 0){
+            menuState = "confirm"
+            playerNameMoveY = height/5 + 36;
+            playerNameMoveReady = true;
+          }
+        }
+      }
+      else{
+        if (playersName.length < 6){
+          playersName += letters[selectedLetter];
+        }
+      }
+    }
+  }
+}
 
 
 function mousePressed(){
@@ -207,6 +358,13 @@ function mousePressed(){
   }
 }
 
+function clickToStart(){
+  background(0);
+  textFont(determinationFont);
+  fill(255);
+  textSize(40);
+  text("Click To Start", width /5, height /4);  
+}
 
 function playCutscene(){
   if (!onceUponATime.isPlaying() && currentFrame !== 11){
@@ -269,13 +427,23 @@ function playCutscene(){
     let img = cutscene[currentFrame];
 
     if (frameTimer > 200 && frameTimer < 560){
-      scrollCutY += 2;
-      if (oUATStartingVolume > 0 && frameTimer > 300){
-        oUATStartingVolume -= 0.0009;
-        onceUponATime.setVolume(oUATStartingVolume); 
+      scrollCutY += 3;
+      if (oUATStartingVolume > 0 && frameTimer > 400){
+        oUATStartingVolume -= 0.0018;
+        onceUponATime.setVolume(oUATStartingVolume);
       }
     }
+
+    if (fadeAlpha < 255 && frameTimer < 200){
+      fadeAlpha += 6;
+    }
+    if (frameTimer > 600){
+      fadeAlpha -= 4;
+    }
+
+    tint(255, fadeAlpha);
     image(img, 0, scrollCutY, width, height * 2);
+    noTint();
 
     fill(0);
     rect(0, 0, width, height /9);
@@ -296,8 +464,9 @@ function playCutscene(){
       if (frameTimer % 60  >= 20){
         textFont(determinationFont);
         fill(170);
-        textSize("30");
-        text("[z]", width/2 - 50, height - 100);
+        textSize("33");
+        textAlign(CENTER);
+        text("[Press z or ENTER]", width/2, height - 200);
       }
       else{
         fill(0);
@@ -306,6 +475,209 @@ function playCutscene(){
     }
   }
 }
+
+function drawCutsceneText(txt){
+  textFont(determinationFont);
+  textSize(40.5);
+  textAlign(LEFT,TOP);
+
+  let xPos = 180;
+  let yPos = height - 225;
+  let boxWidth = width -390;
+
+  fill(255);
+  text(txt, xPos, yPos, boxWidth);
+
+}
+
+function startTitle(){
+  image(title, 0, 0, width, height);
+  if (!startMenuTheme.isPlaying() && !musCymbal.isPlaying()){
+    startMenuTheme.play();   
+  }
+  if (menuState === "instruction"){
+    instructionScreen();
+  }
+  if (menuState === "name"){
+    playerNameScreen();
+  }
+  if (menuState === "confirm"){
+    confirmNameScreen();
+  }
+  if (menuState === "startinggame"){
+    startGameFade();
+  }
+}
+
+function instructionScreen(){
+  fill(190);
+  textSize(33);
+  textFont(determinationFont);
+  textAlign(LEFT);
+
+  text(
+    " ---Instruction---\n" +
+    "\n" +
+    "[Z or ENTER]-Confirm\n" +
+    "[X or SHIFT]-Cancel\n" +
+    "[WASD]-Movement\n" +
+    "[C or CTRL]-Menu(In-game)\n" +
+    "[F]-Fullscreen\n" +
+    "[Hold Q]-Quit\n" +
+    "When HP is 0, you lose.",
+    width /3.5, height /4
+  )
+}
+
+function playerNameScreen(){
+  background(0);
+  textFont(determinationFont);
+  textAlign(CENTER);
+  fill(255);
+
+  textSize(33);
+  text("Name the fallen human.", width / 2, height / 8);
+  
+
+  fill(255);
+  textSize(33);
+  textAlign(CENTER);
+  text(playersName, width/2+ 10, height/5 + 36);
+
+
+  let capStartY = height/3;
+  let lowStartY = capStartY + 3.5 * 45 + 40;
+  let cellWidth = 80;
+  let cellHeight = 45;
+  let startX = width/2 - (letterCols * cellWidth)/2;
+
+  for (let i = 0; i < letters.length; i++){
+
+    let col = i % letterCols;
+    let row = Math.floor(i / letterCols);
+    let lx;
+    let ly;
+
+    if (i < capitals.length){
+      lx = startX + col * cellWidth;
+      ly = capStartY + row * cellHeight;
+    }
+    else{
+
+      let li = i - capitals.length;
+      let lcol = li % letterCols;
+      let lrow = Math.floor(li / letterCols);
+
+      lx = startX + lcol * cellWidth;
+      ly = lowStartY + lrow * cellHeight;
+    }
+
+    if (frameCount % 4 === 0){
+      letterJitterX[i] = random(-1, 1);
+      letterJitterY[i] = random(-1, 1);
+    }
+
+    let jx = letterJitterX[i];
+    let jy = letterJitterY[i];
+
+    if (i === selectedLetter){
+      fill(255, 255, 0);
+    }
+    else{
+      fill(255);
+    }
+
+    textSize(33);
+    textAlign(LEFT);
+    text(letters[i], lx + jx, ly + jy);
+  }
+
+  let buttonY = height - height/8;
+  let buttonSpacing = width/4;
+
+  textSize(33);
+  textAlign(CENTER);
+  for (let i = 0; i < buttons.length; i++){
+    let buttonIndex = letters.length + i;
+    if (buttonIndex === selectedLetter){
+      fill(255, 255, 0);
+    }
+    else{
+      fill(255);
+    }
+    text(buttons[i], buttonSpacing * (i + 1), buttonY);
+  }
+}
+
+function confirmNameScreen(){
+  background(0);
+  textFont(determinationFont);
+  textAlign(CENTER);
+
+  if (frameCount % 4 === 0){
+    nameJitterX = random(-2, 2);
+    nameJitterY = random(-2, 2);
+  }
+
+  let targetY = height - 300;
+
+  if (playerNameMoveReady && playerNameMoveY < targetY){
+    playerNameMoveY += 1;
+  }
+  fill(255);
+  textSize(33);
+  text("The true name.", width/2.2, height/8);
+
+  textSize(textSizeIncrease);
+  text(playersName, width/2 + nameJitterX, playerNameMoveY + nameJitterY);
+  if (textSizeIncrease < 140){
+    textSizeIncrease += 0.35;
+  }
+
+
+  textSize(33);
+  if (confirmSelection === 0){
+    fill(255,255,0);
+  }
+  else{
+    fill(255);
+  }
+  text("No", width /2 - 150, height - 100);
+
+  if (confirmSelection === 1){
+    fill(255, 255, 0);
+  }
+  else{
+    fill(255);
+  }
+  text("Yes", width/2 + 150, height - 100);
+}
+
+function startGameFade(){
+  startMenuTheme.stop();
+  background(0);
+  textFont(determinationFont);
+  textAlign(CENTER);
+
+  if (fadeAlpha === 0 && !musCymbal.isPlaying()){
+    musCymbal.play()
+  }
+
+  fadeAlpha += 255 / 300;
+
+  fill(255);
+  textSize(textSizeIncrease);
+  text(playersName, width/2, playerNameMoveY);
+
+  fill(255, fadeAlpha);
+  rect(0,0,width, height);
+
+  if (fadeAlpha >= 255){
+    fadeAlpha = 0;
+    gameState = "ruins";
+  }
+}
+
 
 function startRuins(){
   background(0);
@@ -319,6 +691,7 @@ function startRuins(){
 
 function playerMove(){
   let moving = false;
+  let newDirection = direction;
 
   if (keyIsDown(65)){
     if (playerX > width/4){
@@ -329,11 +702,11 @@ function playerMove(){
     }
 
     currentSprites = playerSpriteLeft;
-    direction = "left";
+    newDirection = "left";
     moving = true;
   }
 
-  if (keyIsDown(68)){ // right
+  if (keyIsDown(68)){ 
     if (playerX < width - width/4){
       playerX += speed; // move player freely
     }
@@ -342,7 +715,7 @@ function playerMove(){
    }
 
     currentSprites = playerSpriteRight;
-    direction = "right";
+    newDirection = "right";
     moving = true;
   }
   if (keyIsDown(87)){
@@ -354,7 +727,7 @@ function playerMove(){
    }
 
     currentSprites = playerSpriteBack;
-    direction = "back";
+    newDirection = "back";
     moving = true;
   }
   if (keyIsDown(83)){
@@ -366,9 +739,15 @@ function playerMove(){
     }
 
     currentSprites = playerSpriteFront;
-    direction = "front";
+    newDirection = "front";
     moving = true;
   }
+
+  if (newDirection !== direction){
+    frameIndex = 0;
+    frameCountAnim = 0;
+  }
+  direction = newDirection;
 
   //do the animation if moving
   if (moving){
@@ -388,47 +767,19 @@ function displayPlayer(){
     let img = currentSprites[frameIndex];
 
     if (img){
-      image(currentSprites[frameIndex], constrain(playerX,width/4 ,width - width/4,),constrain(playerY, height/4, height - height/4), 40 * 1.5, 60 * 1.5);
+      image(currentSprites[frameIndex], playerX, playerY, 40 * 1.5, 60 * 1.5);
     }
   }
 }
 
-function drawCutsceneText(txt){
-  textFont(determinationFont);
-  textSize(27);
-  textAlign(LEFT,TOP);
+function setupWalls(){
+  walls = [
+    {x: 0, y:0, w:9999, h:50},
+    {x: 0, y: 0, w: 50, h: 9999},
+    {x: 0, y: 1900, w:9999, h:50},
+    {x: 2800, y:0,w:50, h:9999},
+  ];
 
-  let xPos = 120;
-  let yPos = height - 150;
-  let boxWidth = width -260;
-
-  fill(255);
-  text(txt, xPos, yPos, boxWidth);
-
-}
-
-function startTitle(){
-  image(title, 0, 0, 640, 480);
-  if (!startMenuTheme.isPlaying()){
-    startMenuTheme.play();   
-  }
-  playerNameScreen(); 
-}
-
-function playerNameScreen(){
-  fill(255);
-  textSize(22);
-  textFont(determinationFont);
-  text("Name the fallen human.", width /3.7, height/7);
-  text("Quit", width/5, height - height /10);
-  text("Backspace", width/2.5, height - height /10);
-  text("Done", width/1.5, height - height/10);
-
-  //display the letters (temporary: will make them jiggle later)
-  text("A    B    C    D    E    F    G", width /5, height/4);
-  text("H    I    J    K    L    M    N", width /5, height/3.3);
-  text("O    P    Q    R    S    T    U", width /5, height/2.8);
-  text("V    W    X    Y    Z          ", width /5, height/2.4);
 }
 
 function chooseWhatToDoWithEnemy() { //Foo's Function DO NOT TOUCH
